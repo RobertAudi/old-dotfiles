6647f5915d0e55978963d1884ba6eafc0715a974
Aziz Light <aziz@azizlight.me>
2014-01-01 14:21:54 +0100

    Rename/Move some fish functions

--- config/fish/functions/utility/cleanup.fish
+++ config/fish/functions/utility/cleanup.fish
@@ -1,4 +1,4 @@
-function cleanup --description="Delete all .DS_Store files recursively"
+function __cleanup --description="Delete all .DS_Store files recursively"
   find . -name '*.DS_Store' -type f -ls -delete
   and log message activity "User `$USER` removed all the .DS_Store files in $PWD recursively"
 end
--- config/fish/functions/utility/countdir.fish
+++ config/fish/functions/utility/countdir.fish
@@ -1,9 +1,6 @@
-function nod --description="Count sub-directories"
+function countdir --description="Count sub-directories"
   set -l num (/bin/ls -l $argv[1] | grep '^d' | wc -l | sed -e 's/^[ \t]*//')
   set_color yellow
   echo "There are $num sub-directories"
-  set_color blue
-  /bin/ls -l $argv[1] | awk '/^d/ {print $9}' | column
-  echo ' '
   set_color normal
 end
