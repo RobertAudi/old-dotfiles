ccfae790c96ba4386df07fe3d341b9606ffadafc
Aziz Light <aziz@azizlight.me>
2014-01-08 15:13:55 +0100

    ggi is faster to type than cgi

--- config/fish/functions/cgi.fish
+++
@@ -1,7 +0,0 @@
-function cgi --description="Go to the dir of gish"
-  cd $GISH_DIR
-
-  echo ""
-
-  gs
-end
---
+++ config/fish/functions/ggi.fish
@@ -0,0 +1,7 @@
+function ggi --description="Go to the dir of gish"
+  cd $GISH_DIR
+
+  echo ""
+
+  gs
+end
