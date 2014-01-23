d64150f0fa644ef13174a3dadd0e38d5ed62c6b2
Aziz Light <aziz@azizlight.me>
2014-01-23 15:09:51 +0100

    ignore more shit when using the tree command

--- config/fish/functions/tree.fish
+++ config/fish/functions/tree.fish
@@ -1,3 +1,5 @@
 function tree --description="List contents of directories in a tree-like format"
-  command tree -CAFa -I 'rhel.*.*.package|.git|.gem' --dirsfirst $argv
+  set -l ignore "rhel.*.*.package|.git|.gem|*build*|*Pods*"
+  set -l ignore "$ignore|*xc*|*mom*"
+  command tree -CAFa -I "$ignore" --dirsfirst $argv
 end
